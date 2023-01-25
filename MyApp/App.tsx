ad668ff4b272cd84b6cedd2ba592fe916aab1be9
07rameen <rameenhamidd257@gmail.com>
2023-01-25 17:20:04 +0500

    scroll view

--- MyApp/App.tsx
+++ MyApp/App.tsx
@@ -47,13 +47,44 @@ function App(): JSX.Element {
   }
   return (
   <View style={styles.body}>
-    <Text style={styles.text}>{five}</Text>
-    <Text style={styles.text}>{name} </Text>
-    <Text style={styles.text}>This is session No: {session.number} and about {session.title} </Text>
-    <Text style={styles.text}> You clicked : {counter} times </Text>
-    <Text style={styles.text}>{current ? 'current session': 'next session'} </Text>
-     <Button title='Add' //</View>onPress={()=>{Linking.openURL('https://www.youtube.com/')}}
-     onPress={onClickHandler}></Button> 
+    <View style={styles.row}>
+      <View style={styles.view1}>
+      <Text style={styles.text}> 1 </Text>
+      </View> 
+      
+      <View style={styles.view2}>
+      <Text style={styles.text}> 2 </Text>
+      </View> 
+      <View style={styles.view3}>
+      <Text style={styles.text}> 3 </Text>
+    </View>
+    </View>
+
+    <View style={styles.row}>
+      <View style={styles.view4}>
+        <Text style={styles.text}> 4 </Text>
+    </View>
+    </View>
+
+    <View style={styles.row}>
+      <View style={styles.view5}>
+        <Text style={styles.text}> 5 </Text>
+    </View>
+    </View>
+
+    <View style={styles.biggerarea}>
+        <View style={styles.view6}>
+          <Text style={styles.text}>6</Text>
+        </View>
+        <View style={styles.view7}>
+          <Text style={styles.text}>7</Text>
+        </View>
+      </View>
+
+
+
+
+
   </View>
 
   );
@@ -62,14 +93,94 @@ function App(): JSX.Element {
 const styles = StyleSheet.create({
   body:
   {
-    flex: 1,
+  flex:1,
+  flexDirection: 'column',
   backgroundColor: '#000fffff',
-  alignItems: 'center',
+  alignItems: 'flex-start',
   justifyContent: 'center',
+  },
+  row:
+  {
+    flexDirection: 'row',
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  view1:
+  {
+    flex:1,
+    backgroundColor: '#00ffff',
+    alignItems: 'center',
+    justifyContent: 'center'
+
+  },
+  view2:
+  {
+    flex:2,
+    backgroundColor: '#ff00ff',
+    alignItems: 'center',
+    justifyContent: 'center'
+
+  },
+  view3:
+  {
+    flex:3,
+    backgroundColor: '#ffff00',
+    alignItems: 'center',
+    justifyContent: 'center'
+
+  },
+   mainView:
+  {
+    flex:1,
+    flexDirection: 'column',
+    backgroundColor: '#ffff00',
+    alignItems: 'center',
+    justifyContent: 'center'
+
+  },
+   view4:
+  {
+    width:400,
+    height:100,
+    backgroundColor: '#ffff00',
+    alignItems: 'center',
+    justifyContent: 'center'
+
+  },
+   view5:
+  {
+    width:400,
+    height:100,
+    backgroundColor: '#ff0f00',
+    alignItems: 'center',
+    justifyContent: 'center'
+
+  },
+
+  biggerarea: {
+    flex: 1,
+    flexDirection: 'row',
+    backgroundColor: '#ffffff',
+    alignItems: 'stretch',
+    justifyContent: 'center',
+  },
+  view6:{
+     flex: 1,
+    backgroundColor: '#ffffff',
+    alignItems: 'center',
+    justifyContent: 'center',
+
+  },
+  view7:{
+    flex: 1,
+    backgroundColor: '#0000ff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
   text:
   {
-   color: '#ffffff',
+   color: '#000',
    fontSize: 20,
    fontStyle: 'italic',
    margin: 10,
