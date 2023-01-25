5ecb5830f5e7d5576f3647d01675bf4f722ed232
07rameen <rameenhamidd257@gmail.com>
2023-01-25 18:56:48 +0500

    text inputs

--- MyApp/App.tsx
+++ MyApp/App.tsx
@@ -12,6 +12,7 @@ import {
   StatusBar,
   StyleSheet,
   Text,
+  TextInput,
   useColorScheme,
   View,
 } from 'react-native';
@@ -34,100 +35,28 @@ function App(): JSX.Element {
     backgroundColor: isDarkMode ? Colors.darker : Colors.lighter,
   };
 
-  const[name, setName]=useState('Rameen')
-  const [session, setSession]=useState({number: 6, title:'state'})
-  const [current,currentSession]=useState(true)
-  const [counter, setCounter]=useState(0)
-  const [five,setFive]=useState(0)
-
-  const onClickHandler =()=>
-  {
-    setName('Programming with Rameen')
-    setSession({number: 7,title:'Style'})
-    currentSession(false)
-    setCounter(counter+1)
-    setFive(five+5)
-  }
-
-  const[Items,setItems]=useState([
-    {title:'Title One',data:['item 2-1','item 2-2','item 2-3']},
-  ])
+  const [name,setname]=useState('');
 
   
-  const[Refreshing,setRefreshing]=useState(false)
-  const onRefresh =() =>
-  {
-    setRefreshing(true);
-    const adding_index = Items.length + 1;
-    setItems([...Items,{
-      title: 'Title ' + adding_index,
-      data:
-      [
-        'Item ' + adding_index + '-1',
-        'Item ' + adding_index + '-2'
-      ],
-    }]);
-
-    setRefreshing(false);
-
-  }
-
-  return (
-    // <FlatList
-    // // numColumns={2}
-    // // horizontal
-    // keyExtractor={(item,index)=>index.toString()}
-    //   data={Items}
-    //   renderItem={({item})=>(
-    //     <View style={styles.item}>
-    //              <Text style={styles.text}> {item.name} </Text>
-    //     </View>
-
-    //   )}
-    //   refreshControl={
-    //       <RefreshControl
-    //         refreshing={ Refreshing}
-    //           onRefresh={onRefresh}
-    //           colors={['#ff00ff']}
-    //       />
+  return ( 
+    <View style={styles.body}>
+      <Text style={styles.text}> 
+      Please Write Your Name:
+       </Text>
+      <TextInput style={styles.input} placeholder='e.g Rameen'
+      onChangeText={(value)=>setname(value)}
+      // keyboardType='default'
+      // maxLength={3}
+      // editable={false}
+      // secureTextEntry
       
-    //      }/>
-    
-    <SectionList
-    //what is key extractor doing?
-    keyExtractor={(item,index)=>index.toString()}
-    sections={Items}
-    //to display items use render item 
-    renderItem={({item})=>(
-        
-                   <Text style={styles.text}> {item} </Text>
-        //item for showing the nested stuff 1-1 wagera 
-  
-        )}
-        //display title using render section header
-        renderSectionHeader={({section})=>
-        <View style={styles.item}>
-        <Text style={styles.text}> {section.title} </Text>
-        
-        {/* here internal array items are being displayed item.name karnay ki zaroorat nahin */}
+      >
 
-</View>
-
-        
-}
-
-
-refreshControl={
-  <RefreshControl
-    refreshing={ Refreshing}
-      onRefresh={onRefresh}
-      colors={['#ff00ff']}
-  />}
-
-    />
-
-    
-  
+      </TextInput>
+      <Text style={styles.text}> 
+      Your name is : {name}
+       </Text>
+    </View>
 
   );
 }
@@ -136,23 +65,24 @@ const styles = StyleSheet.create({
   body:
   {
   flex:1,
-  flexDirection: 'column',
+  alignItems:'center',
   backgroundColor: '#ffffff',
   },
-  item:
+  input:
   {
-    backgroundColor: '#4ae1fa',
-    alignItems: 'center',
-    justifyContent: 'center',
-    margin: 10
+    width:200,
+    borderWidth:1,
+    borderColor:'#555',
+    borderRadius:5,
+    alignItems:'center',
+    fontSize: 20,
   },
   text:
   {
    color: '#000',
+   margin: 10,
    fontSize: 20,
    fontStyle: 'italic',
-   margin: 10,
-
   },
 });
 
