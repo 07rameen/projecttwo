f0af6ada783470930b2ea4a8c6ad4e5b2f0c3fba
07rameen <rameenhamidd257@gmail.com>
2023-01-25 17:57:14 +0500

    flatlist and section list using nested array yt

--- MyApp/App.tsx
+++ MyApp/App.tsx
@@ -3,9 +3,12 @@ import React , {useState} from 'react';
 import type {PropsWithChildren} from 'react';
 import {
   Button,
+  FlatList,
   Linking,
+  RefreshControl,
   SafeAreaView,
   ScrollView,
+  SectionList,
   StatusBar,
   StyleSheet,
   Text,
@@ -45,47 +48,100 @@ function App(): JSX.Element {
     setCounter(counter+1)
     setFive(five+5)
   }
+
+  const[Items,setItems]=useState([
+    {name:'item 1'},
+    {name:'item 2'},
+    {name:'item 3'},
+    {name:'item 4'},
+    {name:'item 5'},
+    {name:'item 6'},
+    {name:'item 7'},
+    {name:'item 50'},
+    {name:'item 60'},
+    {name:'item 70'},
+    {name:'item 80'},
+    {name:'item 90'}
+
+
+  ])
+
+  const DATA=[
+    {
+      title:'Tile One',
+      data:['Item 1-1','Item 1-2','Item 1-3'],
+
+    },
+    {
+      title:'Tile TWO',
+      data:['Item 2-1','Item 2-2','Item 2-3'],
+
+    },
+    {
+      title:'Tile THREE',
+      data:['Item 3-1','Item 3-2','Item 3-3'],
+
+    },
+    {
+      title:'Tile FOUR',
+      data:['Item 4-1','Item 4-2','Item 4-3'],
+
+    },
+  ]
+
+  const[Refreshing,setRefreshing]=useState(false)
+  const onRefresh =() =>
+  {
+    setRefreshing(true);
+    setItems([...Items,{name:'Key is 69'}]);
+    setRefreshing(false);
+
+  }
+
   return (
-  <View style={styles.body}>
-    <View style={styles.row}>
-      <View style={styles.view1}>
-      <Text style={styles.text}> 1 </Text>
-      </View> 
+    // <FlatList
+    // // numColumns={2}
+    // // horizontal
+    // keyExtractor={(item,index)=>index.toString()}
+    //   data={Items}
+    //   renderItem={({item})=>(
+    //     <View style={styles.item}>
+    //              <Text style={styles.text}> {item.name} </Text>
+    //     </View>
+
+    //   )}
+    //   refreshControl={
+    //       <RefreshControl
+    //         refreshing={ Refreshing}
+    //           onRefresh={onRefresh}
+    //           colors={['#ff00ff']}
+    //       />
       
-      <View style={styles.view2}>
-      <Text style={styles.text}> 2 </Text>
-      </View> 
-      <View style={styles.view3}>
-      <Text style={styles.text}> 3 </Text>
-    </View>
-    </View>
+    //      }/>
+    
+    <SectionList
+    //what is key extractor doing?
+    keyExtractor={(item,index)=>index.toString()}
+    sections={DATA}
+    //to display items use render item 
+    renderItem={({item})=>(
+        
+                   <Text style={styles.text}> {item} </Text>
+        //item for showing the nested stuff 1-1 wagera 
+  
+        )}
+        //display title using render section header
+        renderSectionHeader={({section})=>
+        <View style={styles.item}>
+        <Text style={styles.text}> {section.title} </Text>
+        {/* here internal array items are being displayed item.name karnay ki zaroorat nahin */}
+</View>
+}
 
-    <View style={styles.row}>
-      <View style={styles.view4}>
-        <Text style={styles.text}> 4 </Text>
-    </View>
-    </View>
+    />
 
-    <View style={styles.row}>
-      <View style={styles.view5}>
-        <Text style={styles.text}> 5 </Text>
-    </View>
-    </View>
-
-    <View style={styles.biggerarea}>
-        <View style={styles.view6}>
-          <Text style={styles.text}>6</Text>
-        </View>
-        <View style={styles.view7}>
-          <Text style={styles.text}>7</Text>
-        </View>
-      </View>
-
-
-
-
-
-  </View>
+    
+  
 
   );
 }
@@ -95,88 +151,14 @@ const styles = StyleSheet.create({
   {
   flex:1,
   flexDirection: 'column',
-  backgroundColor: '#000fffff',
-  alignItems: 'flex-start',
-  justifyContent: 'center',
+  backgroundColor: '#ffffff',
   },
-  row:
+  item:
   {
-    flexDirection: 'row',
-    backgroundColor: '#ffffff',
+    backgroundColor: '#4ae1fa',
     alignItems: 'center',
     justifyContent: 'center',
-  },
-  view1:
-  {
-    flex:1,
-    backgroundColor: '#00ffff',
-    alignItems: 'center',
-    justifyContent: 'center'
-
-  },
-  view2:
-  {
-    flex:2,
-    backgroundColor: '#ff00ff',
-    alignItems: 'center',
-    justifyContent: 'center'
-
-  },
-  view3:
-  {
-    flex:3,
-    backgroundColor: '#ffff00',
-    alignItems: 'center',
-    justifyContent: 'center'
-
-  },
-   mainView:
-  {
-    flex:1,
-    flexDirection: 'column',
-    backgroundColor: '#ffff00',
-    alignItems: 'center',
-    justifyContent: 'center'
-
-  },
-   view4:
-  {
-    width:400,
-    height:100,
-    backgroundColor: '#ffff00',
-    alignItems: 'center',
-    justifyContent: 'center'
-
-  },
-   view5:
-  {
-    width:400,
-    height:100,
-    backgroundColor: '#ff0f00',
-    alignItems: 'center',
-    justifyContent: 'center'
-
-  },
-
-  biggerarea: {
-    flex: 1,
-    flexDirection: 'row',
-    backgroundColor: '#ffffff',
-    alignItems: 'stretch',
-    justifyContent: 'center',
-  },
-  view6:{
-     flex: 1,
-    backgroundColor: '#ffffff',
-    alignItems: 'center',
-    justifyContent: 'center',
-
-  },
-  view7:{
-    flex: 1,
-    backgroundColor: '#0000ff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    margin: 10
   },
   text:
   {
