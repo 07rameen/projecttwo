52e3cae2b0c62e5b18ae12a5442b80d3315b86a7
07rameen <rameenhamidd257@gmail.com>
2023-02-06 17:05:56 +0500

    Tab Navigation -errors

--- MyApp/App.tsx
+++ MyApp/App.tsx
@@ -1,102 +1,68 @@
 
 import React , {useState} from 'react';
-import type {PropsWithChildren} from 'react';
-import {
-  Pressable,
-  StyleSheet,
-  Text,
-  View,
-} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
 import 'react-native-gesture-handler';
+import ScreenA from './ScreenA';
+import ScreenB from './ScreenB';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import FontAwesome5 from 'react-native-vector-icons/FontAwesome5';
+import { createMaterialBottomTabNavigator } from '@react-navigation/material-bottom-tabs';
+import { createMaterialTopTabNavigator } from '@react-navigation/material-top-tabs';
 
-const Stack= createStackNavigator();
+//const Tab=createBottomTabNavigator();
+const Tab=createMaterialBottomTabNavigator();
+//const Tab=createMaterialTopTabNavigator();
 
-function ScreenA({navigation}: {navigation: any})
-{
-  const onPressHandler=()=>
-  {
-    //navigation.navigate('Root', { screen: 'Settings' });
-    navigation.navigate('ScreenB');
-    //navigation.goback //navigation.replace
-
-  }
-  return(
-    <View style={styles.body}>
-      <Text style={styles.text}>
-        Screen A 
-      </Text>
-      <Pressable 
-      onPress={onPressHandler}
-      style={({pressed})=>({backgroundColor: pressed? '#ddd':'#0f0'})}>
-          <Text style={styles.text}>
-          Go to screen B
-        </Text>
-      </Pressable>
-    </View>
-  )
-
-}
-
-function ScreenB({navigation}: {navigation: any}) {
-
-  const onPressHandler = () => {
-    // navigation.navigate('Screen_A');
-    navigation.goBack();
-  }
-
+function App() {
   return (
-    <View style={styles.body}>
-      <Text style={styles.text}>
-        Screen B
-      </Text>
-      <Pressable
-        onPress={onPressHandler}
-        style={({ pressed }) => ({ backgroundColor: pressed ? '#ddd' : '#0f0' })}
-      >
-        <Text style={styles.text}>
-          Go Back to Screen A
-        </Text>
-      </Pressable>
-    </View>
-  )
-}
-
-function App()
-{
-  return(
     <NavigationContainer>
-      <Stack.Navigator>
-          <Stack.Screen
-          name="ScreenA"
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, size, color }) => {
+            let iconName;
+            if (route.name === 'Screen_A') {
+              iconName = 'autoprefixer';
+              size = focused ? 25 : 20;
+              // color = focused ? '#f0f' : '#555';
+            } else if (route.name === 'Screen_B') {
+              iconName = 'btc';
+              size = focused ? 25 : 20;
+              // color = focused ? '#f0f' : '#555';
+            }
+            return (
+              <FontAwesome5
+                name={iconName}
+                size={size}
+                color={color}
+              />
+            )
+          }
+        })}
+        tabBarOptions={{
+          activeTintColor: '#f0f',
+          inactiveTintColor: '#555',
+          activeBackgroundColor: '#fff',
+          inactiveBackgroundColor: '#999',
+          showLabel: true,
+          labelStyle: { fontSize: 14 },
+          showIcon: true,
+        }}
+        activeColor='#f0edf6'
+        inactiveColor='#3e2465'
+        barStyle={{ backgroundColor: '#694fad' }}
+      >
+        <Tab.Screen
+          name="Screen_A"
           component={ScreenA}
-          //options={{header:()=>null}}
-          />
-          <Stack.Screen
-           name="ScreenB"
-           component={ScreenB}
-          />
-      </Stack.Navigator>
+          options={{ tabBarBadge: 3 }}
+        />
+        <Tab.Screen
+          name="Screen_B"
+          component={ScreenB}
+        />
+      </Tab.Navigator>
     </NavigationContainer>
   )
-
 }
-
-const styles=StyleSheet.create({
-body:
-{
-flex:1,
-justifyContent: 'center',
-alignItems: 'center',
-},
-text:
-{
-  fontSize:40,
-  fontWeight:'bold',
-  margin: 10,
-}
-
-})
 
 export default App;
